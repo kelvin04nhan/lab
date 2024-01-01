bb6a6b7443678d2b721dd8663c25c71e31af7b9a
kelvin04nhan <thanhnhan13082021@gmail.com>
2024-01-01 14:37:19 +0700

    oop

---
+++ washing-machine/view/washer.py
@@ -0,0 +1,18 @@
+# from view.washer import Washer
+# class Simulator:
+#     def __init__(self):
+#         self.__washer = Washer()
+        
+#     def run(self):
+#         while True:
+#             self.__car.display_menu()
+#             choice = input("Enter your command: ")
+#             for command in choice:
+#                 if command == '0':
+#                     return 
+#                 self.process_menu(command)
+                
+                
+#     def process_menu(self, command):
+#         if command == '1':
+#             self.__washer.
